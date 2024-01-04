14a7d6168d5b2b2e09e1379a56732c7d8e365599
MYCOM <MYCOM@DESKTOP-DK8REN4>
2024-01-04 10:27:48 +0900

    Vo 클래스 추가

--- jspfolder/src/main/java/vo/Member.java
+++ jspfolder/src/main/java/vo/Member.java
@@ -1,5 +1,75 @@
 package vo;
 
 public class Member {
-
+	private int mno;
+	private String mid;
+	private String mpw;
+	private String mnickNm;
+	private String mname;
+	private int mbirth;
+	private String mphone;
+	private char mgender;
+	private String memail;
+	private String mrdate;
+	
+	public int getMno() {
+		return mno;
+	}
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getMpw() {
+		return mpw;
+	}
+	public void setMpw(String mpw) {
+		this.mpw = mpw;
+	}
+	public String getMnickNm() {
+		return mnickNm;
+	}
+	public void setMnickNm(String mnickNm) {
+		this.mnickNm = mnickNm;
+	}
+	public String getMname() {
+		return mname;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+	public int getMbirth() {
+		return mbirth;
+	}
+	public void setMbirth(int mbirth) {
+		this.mbirth = mbirth;
+	}
+	public String getMphone() {
+		return mphone;
+	}
+	public void setMphone(String mphone) {
+		this.mphone = mphone;
+	}
+	public char getMgender() {
+		return mgender;
+	}
+	public void setMgender(char mgender) {
+		this.mgender = mgender;
+	}
+	public String getMemail() {
+		return memail;
+	}
+	public void setMemail(String memail) {
+		this.memail = memail;
+	}
+	public String getMrdate() {
+		return mrdate;
+	}
+	public void setMrdate(String mrdate) {
+		this.mrdate = mrdate;
+	}
 }
